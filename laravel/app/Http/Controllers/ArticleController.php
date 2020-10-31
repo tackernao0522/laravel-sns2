a67cc5b5284ff2aa0735b650156e53c19a110a3d
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2020-10-31 20:32:43 +0900

    ArticleController@indexの修正 全共通画面の作成(app.blade.php) 記事一覧画面の作成(articles/index.blade.php) ヘッダーナビゲーションバーの作成(nav.blade.php)

--- laravel/app/Http/Controllers/ArticleController.php
+++ laravel/app/Http/Controllers/ArticleController.php
@@ -41,6 +41,6 @@ class ArticleController extends Controller
             ],
         ];
 
-        return view('article.index', ['articles' => $articles]);
+        return view('articles.index', ['articles' => $articles]);
     }
 }
