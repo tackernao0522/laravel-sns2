0b6c149e853bafb2f49d6bcb8308547cc596993f
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2020-11-01 12:57:59 +0900

    ログアウト可能にする為にnav.blade.phpを編集

--- laravel/resources/views/nav.blade.php
+++ laravel/resources/views/nav.blade.php
@@ -3,18 +3,26 @@
   <a class="navbar-brand" href="/"><i class="far fa-sticky-note mr-1"></i>memo</a>
 
   <ul class="navbar-nav ml-auto">
-    <li class="nav-item">
-      <a class="nav-link" href="">ユーザー登録</a>
-    </li>
 
+    @guest
+    <li class="nav-item">
+      <a class="nav-link" href="{{ route('register') }}">ユーザー登録</a>
+    </li>
+    @endguest
+
+    @guest
     <li class="nav-item">
       <a class="nav-link" href="">ログイン</a>
     </li>
+    @endguest
 
+    @auth
     <li class="nav-item">
       <a class="nav-link" href=""><i class="fas fa-pem mr-1"></i>投稿する</a>
     </li>
+    @endauth
 
+    @auth
     <!-- Dropdown -->
     <li class="nav-item dropdown">
       <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanted="false">
@@ -30,9 +38,11 @@
         </button>
       </div>
     </li>
-    <form id="logout-button" method="POST" action="">
+    <form id="logout-button" method="POST" action="{{ route('logout') }}">
+      @csrf
     </form>
     <!-- Dropdown -->
+    @endauth
 
   </ul>
 
