1850441c9964bc3d3e198d62c3ca4a24e4797cda
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2020-11-03 16:19:19 +0900

    いいねした記事一覧を表示する

--- laravel/app/User.php
+++ laravel/app/User.php
@@ -61,6 +61,11 @@ class User extends Authenticatable
         return $this->belongsToMany('App\User', 'follows', 'follower_id', 'followee_id')->withTimestamps();
     }
 
+    public function likes(): BelongsToMany
+    {
+        return $this->belongsToMany('App\Article', 'likes')->withTimestamps();
+    }
+
     public function isFollowedBy(?user $user): bool
     {
         return $user
